8b96e6f4337d3196eb27d9323e1ef01dd09ea4c3
rashidpkn <muhammed916rashid@gmail.com>
2022-11-22 04:42:56 +0000

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+
 function App() {
   return (
     <div className="min-h-screen w-full flex flex-col lg:flex-row justify-center items-center  relative text-white">
-      <img src="/image/banner.jpg" class="h-full w-full object-top object-cover brightness-50 absolute inset-0 mx-auto -z-10" alt="" />
+      <div className="h-full w-full brightness-50 absolute inset-0 mx-auto -z-10 overflow-hidden">
+
+
+        <video className='hidden lg:block h-full w-full object-cover' playsInline loop="true" autoplay="autoplay" muted>
+          <source type="video/mp4" src="/video/cover.mp4"></source>
+          <source type="video/ogg" src="/video/cover.ogg"></source>
+        </video>
+
+        <img src="/image/banner.jpg" class="lg:hidden h-full w-full object-cover" alt="" />
+
+      </div>
 
       <Left />
 
@@ -15,11 +26,24 @@ function App() {
 export default App
 
 
+
+function BGVideo() {
+  return (
+
+    <video className='hidden lg:flex' width={'100%'} height={"100%"} loop autoplay muted>
+      <source src='/video/cover.mp4' type="video/mp4" />
+      <source src='/video/cover.webm' type="video/webm" />
+      <source src='/video/cover.ogg' type="video/ogg" />
+      Your browser does not support the video tag.
+    </video>
+  )
+}
+
 function Left() {
   return (
     <div className="h-full w-full lg:w-[35%] flex flex-col gap-5 justify-center items-center py-5">
       <img src="/image/logo.png" className='w-60' alt="" />
-      
+
     </div>
   )
 }
@@ -30,28 +54,14 @@ function Right() {
   return (
     <div className="h-full w-full lg:w-[65%] flex flex-col items-center lg:items-start mg:justify-center px-5 text-white gap-10">
       <div className="flex items-center gap-5">
-        <div className="h-[1px] w-10 bg-[#ec008c]" /> <h3>WELCOME TO COUNT</h3>
+        <div className="h-[1px] w-10 bg-[#2A3812]" /> <h3>Welcome to BAM BAM BUDZ</h3>
       </div>
-      <h2 className='text-7xl lg:w-3/4'>We are currently working
-        on a new super awesome
-        website.</h2>
-      <p className='opacity-30 lg:w-3/4'>Nulla porttitor accumsan tincidunt. Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat.</p>
+      <h2 className='text-7xl lg:w-3/4 text-center lg:text-start'>Stay tuned. We are launching soon..!</h2>
+      {/* <p className='opacity-30 lg:w-3/4'>Welcome to BAM BAM BUDZ</p> */}
       <div className="h-14 w-96 flex">
         <input className='bg-white opacity-25 h-full w-4/6 outline-none text-black pl-3' type="text" />
-        <button className='h-full w-2/4 bg-[#ec008c]'>NOTIFY ME</button>
+        <button className='h-full w-2/4 bg-[#2A3812]'>NOTIFY ME</button>
       </div>
     </div>
   )
 }
-//  <div className="flex items-center gap-5">
-//         <div className="h-[1px] w-10 bg-[#ec008c]" /> <h3>LAUNCHING IN</h3>
-//       </div>
-//       <div className="flex relative gap-3">
-//         <h2 className='text-9xl'>00</h2>
-//         <span className='bg-[#ec008c] absolute -right-14 bottom-0 text-2xl font-semibold p-1 px-2 rounded-md '>day</span>
-//       </div>
-//       <div className="w-full flex justify-center gap-7">
-//         <span className='text-3xl'>00 <span className='text-xl text-white/70'>H</span></span>
-//         <span className='text-3xl'>00 <span className='text-xl text-white/70'>M</span></span>
-//         <span className='text-3xl'>00 <span className='text-xl text-white/70'>S</span></span>
-//       </div>
